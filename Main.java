bf637d7edfffd510730ac897a14c07c432c5df2d
gowthaman0304 <gowthamg099@gmail.com>
2022-10-11 19:42:36 +0530

    private keyword program

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  private String fname = "Gowthaman";
+  private String lname = "G";
+  private String email = "gowthamg099@gmail.com";
+  private int age = 21;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Email: " + myObj.email);
+    System.out.println("Age: " + myObj.age);
+  }
+}
+
